7a94e2b40c917677ed516c095f6cf091afe194d4
kartik-270 <kartik2310179@akgec.ac.in>
2024-12-25 22:51:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -230,4 +230,6 @@ def plan_trip_route():
     return jsonify(full_itinerary)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
+    # app.run(debug=True)
